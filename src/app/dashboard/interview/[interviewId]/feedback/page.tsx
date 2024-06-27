18073ef58faa71820ff1dec6d1e3f4cd228b06fc
anagh-azad <anagh.azad.ug20@nsut.ac.in>
2024-06-27 20:42:24 +0530

    added interview feedback page

--- src/app/dashboard/interview/[interviewId]/feedback/page.tsx
+++ src/app/dashboard/interview/[interviewId]/feedback/page.tsx
@@ -1,7 +1,99 @@
-import React from "react";
+"use client";
+
+import { db } from "@/utils/db";
+import { UserAnswer } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import { useParams, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger
+} from "@/components/ui/collapsible";
+import { ChevronsUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+type feedbackType = {
+  id: number;
+  createdAt: string | null;
+  mockIdRef: string;
+  question: string;
+  correctAns: string | null;
+  userAns: string | null;
+  feedback: string | null;
+  rating: string | null;
+  userEmail: string | null;
+}[];
 
 function Feedback() {
-  return <div>Feedback</div>;
+  const params = useParams();
+  const interviewId = params.interviewId.toString();
+  const router = useRouter();
+
+  const [feedbackList, setFeedbackList] = useState<feedbackType>([]);
+
+  useEffect(() => {
+    getFeedback();
+  }, []);
+
+  const getFeedback = async () => {
+    const result = await db
+      .select()
+      .from(UserAnswer)
+      .where(eq(UserAnswer.mockIdRef, interviewId))
+      .orderBy(UserAnswer.id);
+
+    console.log("Feedback result from db:", result);
+    setFeedbackList(result);
+  };
+
+  return (
+    <div className="p-10">
+      <h2 className="text-3xl font-bold text-green-500">Congratulations!</h2>
+      <h2 className="font-bold text-2xl">Here is your interview feedback</h2>
+      <h2 className="text-blue-500 text-lg my-3">
+        Your overall interview rating : <strong>7</strong>
+      </h2>
+
+      <h2 className="text-sm text-gray-600">
+        Find below the interview question with your answer, correct answer and
+        feedback for improvement
+      </h2>
+      {feedbackList &&
+        feedbackList.map((item, index) => (
+          <Collapsible key={index} className="mt-7">
+            <CollapsibleTrigger className="p-2 bg-secondary rounded-lg flex justify-between my-2 text-left gap-7 w-full">
+              {item.question} <ChevronsUpDown className="h-5 w-5" />
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <div className="flex flex-col gap-2">
+                <h2 className="text-red-500 p-2 border rounded-lg">
+                  <strong>Rating : </strong>
+                  {item.rating}
+                </h2>
+
+                <h2 className="p-2 border rounded-lg bg-blue-50 text-sm text-blue-600">
+                  <strong>Your Answer : </strong>
+                  {item.userAns}
+                </h2>
+                <h2 className="p-2 border rounded-lg bg-green-50 text-sm text-green-700">
+                  <strong>Correct Answer : </strong>
+                  {item.correctAns}
+                </h2>
+                <h2 className="p-2 border rounded-lg bg-gray-50 text-sm text-gray-700">
+                  <strong>Feedback : </strong>
+                  {item.feedback}
+                </h2>
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        ))}
+
+      <Button onClick={() => router.replace("/dashboard")}>
+        Go to Dashboard
+      </Button>
+    </div>
+  );
 }
 
 export default Feedback;
